794c2d7a0aafad86cebabff8172d128fe0fdceaa
Hugo <hugo.arenas@usach.cl>
2019-09-26 01:06:18 -0300

    función convolución bidireccional

--- hijo.c
+++ hijo.c
@@ -8,7 +8,33 @@
 #include "listf.h"
 
 matrixF *bidirectionalConvolution(matrixF *mf, matrixF *filter){
-	if ((countFil(filter) == countColumn(filter))&&(countFil(filter)%3==0)){
-		
+	if ((countFil(filter) == countColumn(filter))&&(countFil(filter)%2 == 1)){
+		int increase = 0, initial = countFil(filter);
+		while (initial != 1){
+			initial = initial - 2;
+			increase = increase + 1;
+		}
+		for (int cont = 0; cont < increase; cont++){
+			mf = amplifyMF(mf);
+		}
+		for (int fil = 0; fil < countFil(filter) - countFil(filter); fil++){
+			for (int col = 0; col < countColumn(filter) - countColumn(filter); col++){
+				float sum = 0.0000;
+				for (int y = 0; y < countFil(filter); y++){
+					for (int x = 0; x < countColumn(filter); x++){
+						float result = getDateMF(mf,y,x)*getDateMF(filter,y + fil,x + col);
+						sum = sum + result;
+					}
+				}
+				mf = setDateMF(mf, fil + (countFil(filter)/2), col + (countColumn(filter)/2), sum);
+			}
+		}
+		for (int cont2 = 0; cont2 < increase; cont2++){
+			mf = decreaseMF(mf);
+		}
+		return mf
+	}
+	else{
+		return mf;
 	}
 }
