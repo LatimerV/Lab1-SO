217370fb321d6f03082fb0792405dad6c4701629
Hugo <hugo.arenas@usach.cl>
2019-09-28 22:33:05 -0300

    matriz de fichero filtro

--- main.c
+++ main.c
@@ -6,8 +6,48 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "matrixf.h"
+#include "listf.h"
 //#include "funct.h"
 //#include "structs.h"
+
+matrixF *convertFilter(char **datefilter, int cont){
+	int colfilter = 0;
+	for (int x = 0; x < strlen(datefilter[0]); x++){
+		if ((datefilter[0][x] == '-') && (datefilter[0][x] == '0') && (datefilter[0][x] == '1') 
+			&& (datefilter[0][x] == '2') && (datefilter[0][x] == '3') && (datefilter[0][x] == '4') 
+		&& (datefilter[0][x] == '5') && (datefilter[0][x] == '6') && (datefilter[0][x] == '7') 
+		&& (datefilter[0][x] == '8') && (datefilter[0][x] == '9')){
+			colfilter = colfilter + 0;
+		}
+		else if (datefilter[0][x] == ' '){
+			colfilter = colfilter + 1;
+		}
+	}
+	matrixF *filter = createMF(cont, colfilter);
+	int fil = 0, col = 0, pos = 0;
+	char *digit = (char *)malloc(10*sizeof(char));
+	for (int a = 0; a < cont; a++){
+		col = 0;
+		for (int b = 0; b < colfilter; b++){
+			if ((datefilter[a][b] == '-') && (datefilter[a][b] == '0') && (datefilter[a][b] == '1') 
+			&& (datefilter[a][b] == '2') && (datefilter[a][b] == '3') && (datefilter[a][b] == '4') 
+		&& (datefilter[a][b] == '5') && (datefilter[a][b] == '6') && (datefilter[a][b] == '7') 
+		&& (datefilter[a][b] == '8') && (datefilter[a][b] == '9')){
+			digit[pos] = datefilter[a][b];
+			pos = pos + 1;
+			}
+			else if (datefilter[a][b] == ' '){
+				pos = 0;
+				filter = setDateMF(filter, fil, col, (atoi(digit)) * 1.0000);
+				col = col + 1;
+				digit = (char *)malloc(10*sizeof(char));
+			}
+		}
+		fil = fil + 1;
+	}
+	return filter;
+}
 
 int main(int argc, char *argv[]){
 
@@ -49,7 +89,32 @@ int main(int argc, char *argv[]){
             
         }   
     }
-    if(aux==1){
+	char **datefilter = (char **)malloc(2000*sizeof(char *));
+	char *date = (char *)malloc(2000*sizeof(char));
+	FILE *filefilter = fopen(mflag,"r");
+	int error = 0, cont = 0;
+	while(error == 0){
+		fseek(filefilter, 0, SEEK_END);
+		if ((filefilter == NULL) || (ftell(filefilter) == 0)){
+			perror("Error en lectura. Ingrese el nombre de un archivo existente.\n");
+			error = 1;
+		}
+		else{
+			date=(char*)malloc(2000*sizeof(char));
+			rewind(filefilter);
+			while(feof(filefilter) == 0){
+				date = fgets(date, 1000, filefilter);
+				datefilter[cont] = date;
+				date = (char*)malloc(1000*sizeof(char));
+				cont = cont + 1;
+			}
+			error = 1;
+		}
+	}
+	rewind(filefilter);
+	fclose(filefilter);
+	matrixF *filter = convertFilter(datefilter, cont - 1);
+    if(aux == 1){
     }
 
 
